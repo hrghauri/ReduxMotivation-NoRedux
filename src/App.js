9653a3ac51c49ae1df1f6d4f610ec1db881ba3c2
Haris Ghauri <hrghauri@hotmail.com>
2018-12-19 19:07:38 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './components/Form';
+import List from './components/List';
+import Number from './components/Number';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      names: []
+    }
+
+    this.addName = this.addName.bind(this);
+    this.removeName = this.removeName.bind(this);
+  }
+
+  addName(name){
+    console.log(name);
+    let names = this.state.names;
+    names.push(name);
+    this.setState({
+      names: names
+    })
+  }
+
+
+  removeName(index){
+    let names = this.state.names;
+    names.splice(index,1);
+    this.setState({names: names})
+  }
+
   render() {
+    if (this.state.names.length === 0)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Form addNameToApp={this.addName}>
+      </Form>
+    )
+
+
+
+
+    const renderedContent = <div>
+      <Form initialName={""}  addNameToApp={this.addName}>
+      </Form>
+
+       
+       <List names={this.state.names} removeNameFromApp = {this.removeName}></List> 
+       <Number length = {this.state.names.length}></Number>
+    </div>
+
+    return (
+      <div>
+      {renderedContent}
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,41 @@
+
+
+import React, { Component } from 'react'
+
+export default class Form extends Component {
+
+    constructor(props){
+        super(props)
+        
+   
+        this.state = {
+            name: ''
+        }
+
+        this.submitForm = this.submitForm.bind(this);
+    }
+
+
+
+submitForm(e){
+    e.preventDefault();
+    console.log('event occured')
+    const name = this.state.name;
+   this.props.addNameToApp(name);
+   this.setState({
+       name: ''
+   })
+  
+}
+
+
+  render() {
+    return (
+        <form onSubmit={this.submitForm}>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" value ={this.state.name} onChange={(e)=>{this.setState({name: e.target.value})}}></input>
+            <input type="submit"></input>
+        </form>
+    )
+  }
+}
